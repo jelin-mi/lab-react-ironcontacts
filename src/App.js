91860891f6a790eb09f7f5f35041c63260b2e518
“jelin.mi” <jelin.mi@gmail.com>
2022-02-04 20:49:37 +0100

    Iteration 1 done w/o css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import contacts from "./contacts.json";
+
+let firstFive = contacts.slice(0, 5);
 
 function App() {
+  const [contacts, setContacts] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <tr>
+        <td>Picture</td>
+        <td>Name</td>
+        <td>Popularity</td>
+      </tr>
+      {firstFive.map((contact) => {
+        return (
+          <table key={contact.id} className="list">
+          
+            <tr >
+              <td><img src={contact.pictureUrl} alt=""/></td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+            </tr>
+          </table>
+        );
+      })}
     </div>
   );
 }
