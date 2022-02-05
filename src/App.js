0af43b7f4c7ff9214b339d98c8e2d94b8ac3a563
“jelin.mi” <jelin.mi@gmail.com>
2022-02-05 18:21:41 +0100

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -3,52 +3,75 @@ import "./App.css";
 import contacts from "./contacts.json";
 
 function App() {
+  // Iteration 1 | Display 5 Contacts
   const [contactsList, setContacts] = useState(contacts.slice(0, 5));
 
+  // Iteration 3 | Add New Random Contacts
   const addRandomContact = () => {
     const randomIndex = Math.floor(Math.random() * (contacts.length - 1));
     const randomContact = contacts[randomIndex];
     const newContactList = [randomContact, ...contactsList];
 
-    if (!contactsList.includes(randomContact)) {
+    !contactsList.includes(randomContact)
+      ? setContacts(newContactList)
+      : addRandomContact();
+
+    /*  if (!contactsList.includes(randomContact)) {
       setContacts(newContactList);
     } else {
       addRandomContact();
-    }
+    } */
+  };
 
-    
+  // Iteration 4 | Sort Contacts by Name and Popularity
+  const sortByPopularity = () => {
+    const popularitySorted = contactsList.sort(
+      (a, b) => b.popularity - a.popularity
+    );
+    setContacts([popularitySorted, ...contactsList]);
+  };
 
-    
+  const sortByName = () => {
+    const nameSorted = contactsList.sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    setContacts([...nameSorted]);
   };
 
   return (
     <div className="App">
       <h1>IronContacts</h1>
       <button onClick={addRandomContact}>Add Random Contact</button>
-      <div className="titles">
-        <h2>Picture</h2>
-        <h2>Name</h2>
-        <h2>Popularity</h2>
-        <h2>Won an Oscar</h2>
-        <h2>Won an Emmy</h2>
-      </div>
-      {contactsList.map((contact) => {
-        return (
-          <table key={contact.id} className="list">
-            <tbody>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <button onClick={sortByName}>Sort by name</button>
+      <table>
+        <thead className="titles">
+          <tr>
+            <td>Picture</td>
+            <td>Name</td>
+            <td>Popularity</td>
+            <td>Won an Oscar</td>
+            <td>Won an Emmy</td>
+          </tr>
+        </thead>
+        {contactsList.map((contact) => {
+          return (
+            <tbody key={contact.id} className="list">
               <tr>
                 <td>
                   <img src={contact.pictureUrl} alt="" />
                 </td>
                 <td>{contact.name}</td>
                 <td>{contact.popularity}</td>
+
+                {/* Iteration 2 | Conditionally Display Awards Info */}
                 <td>{contact.wonOscar === true && "🏆"}</td>
                 <td>{contact.wonEmmy === true && "🏆"}</td>
               </tr>
             </tbody>
-          </table>
-        );
-      })}
+          );
+        })}
+      </table>
     </div>
   );
 }
